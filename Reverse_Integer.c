a0809a4fa7eeda219f0b4a35bf151e7d2ee58151
luck <lucklgh@126.com>
2017-07-06 15:13:11 +0800

    test again

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//test of push 
 int reverse(int x); 
 
 int main(int argc, char *argv[]) 
--- tow_sum.c
+++ tow_sum.c
@@ -1,3 +1,4 @@
+<<<<<<< c4ef75fbd52b71f1494cf99a97365772d904b17f
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -32,3 +33,39 @@ int* twoSum(int* nums, int numsSize, int target)
 	}
 	return (results);
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int* twoSum(int* nums, int numsSize, int target);
+
+int main(int argc, char *argv[]) 
+{
+	int * results;
+	int nums[6] = {-1,-2,-3,-4,-5};
+	results = twoSum(nums,5,-8);
+	printf("%d,%d",results[0],results[1]);
+	return 0;
+}
+
+int* twoSum(int* nums, int numsSize, int target) 
+{
+    int i, j, find;
+    int* results = (int*)malloc(sizeof(int)*2);
+    for (i = 0;i < numsSize;i++)
+    {
+    		for (j = i+1;j < numsSize;j++)
+    		{
+    			if (target == nums[i] + nums[j])
+    			{
+    				results[0] = i;
+    				results[1] = j;
+    				
+				}
+			}
+			    
+	}
+	return (results);
+}
+>>>>>>> test of push
